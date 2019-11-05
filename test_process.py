cbdc11762eef8d1986cc177edc48a5ca92f01b63
Peter Turczak <p_turczak@wiwa.de>
2019-11-05 21:51:12 +0100

    Use visitor primitive

--- test_process.py
+++ test_process.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import logging
 import pickle
+import lark
 
 data = pickle.load(open("tempfile", "rb"))
 
@@ -29,10 +30,69 @@ def process(element, files, lines, level=0):
     else:
         for child in element.children:
             process(child, files, lines, level+1)
+
+
+class dataVisitor(lark.Visitor):
+    def __init__(self):
+        lark.Visitor.__init__(self)
+        self.files = {}
+        self.pages = {}
+        self.page = 0
+
+    def inputline(self, tree):
+        inputtag = tree.children[0].children[0].strip()
+        filename = tree.children[1].strip()
+        assert inputtag not in files
+        self.files[inputtag] = filename
+
+    def sheet(self, tree):
+        self.page += 1
+        self.pages[self.page] = {}
+        print("sheet")
+    
+    def link(self, tree):
+        self.cur_file = int(tree.children[0].children[0])
+        self.cur_line = int(tree.children[1].children[0])
+
+    def _handle_wh(self, tree):
+        width = int(tree.children[0].children[0])
+        height = int(tree.children[1].children[0])
+        return (width, height)
+
+    def size(self, tree):
+        self.cur_size = self._handle_wh(tree)
+
+    def point(self, tree):
+        self.cur_point = self._handle_wh(tree)
+
+    def vboxsection(self, tree):
+        print("vboxsection ")
+        pass
+
+    def voidvboxrecord(self, tree):
+        print("voidvboxrecord")
+        pass
+
+    def voidhboxrecord(self, tree):
+        print("voidhboxrecord")
+        pass
+
+    def hboxsection(self, tree):
+        print("hboxsection")
+        pass
+
+    def NEWLINE(self, tree):
+        print ("NEWLINE")
+
 # Dict inputtag -> filename
 files = {}
 # Dict {inputtag -> Dict { line -> (X,Y,W,H) } }
 lines = {}
 
 #pretty_print(data)
-process(data, files, lines)
+#process(data, files, lines)
+dataVisitor().visit(data)
+
+#print(lines)
+#with open("x", "wb") as f:
+#    pickle.dump( {"files":files, "lines":lines} , f)
