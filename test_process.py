7bcf82b4a16c96b0e6ba45a25a89f517e2038a39
Peter Turczak <p_turczak@wiwa.de>
2019-11-07 16:03:20 +0100

    Butcher parser a bit to improve box detection

--- test_process.py
+++ test_process.py
@@ -59,31 +59,69 @@ class dataVisitor(lark.Visitor):
         height = int(tree.children[1].children[0])
         return (width, height)
 
-    def size(self, tree):
+    def _size(self, tree):
         self.cur_size = self._handle_wh(tree)
+        #print("size {}".format(self.cur_size))
 
-    def point(self, tree):
+    def _point(self, tree):
         self.cur_point = tuple([int(x) for x in tree.children[0].children])
+        #print("point {}".format(self.cur_point))
+
+    def _shipout_box(self, cur_file, cur_line, cur_point, cur_size):
+        pass
 
     def _out_box(self):
-        return (self.cur_file, self.cur_line, self.cur_point, self.cur_size)
+        '''
+        Output a box to the underlying output system
+        '''
+        args = (self.cur_file, self.cur_line, self.cur_point, self.cur_size)
+        self._shipout_box(*args)
+        return args
 
     def vboxsection(self, tree):
-        print("vboxsection {}".format(self._out_box()))
+        return
+        print("vboxsection")
+        if 0 not in self.cur_size:
+            print("vboxsection {}".format(self._out_box()))
 
     def voidvboxrecord(self, tree):
-        print("voidvboxrecord {}".format(self._out_box()))
+        #print("voidvboxrecord {}".format(self._out_box()))
+        pass
 
     def voidhboxrecord(self, tree):
-        print("voidhboxrecord {}".format(self._out_box()))
+        #print("voidhboxrecord {}".format(self._out_box()))
         pass
 
     def hboxsection(self, tree):
-        print("hboxsection {}".format(self._out_box()))
+        #print("hboxsection {}".format(tree))
+        self._point(tree.children[1])
+        self._size(tree.children[2])
+        if 0 not in self.cur_size:
+            print("hboxsection {}".format(self._out_box()))
         pass
 
     def NEWLINE(self, tree):
         print ("NEWLINE")
+
+
+class cairoVisitor(dataVisitor):
+    import cairo
+
+    def __init__(self):
+        dataVisitor.__init__(self)
+        self._surface = self.cairo.PDFSurface("/tmp/x.pdf", 1000, 1000)
+        self._ctx = self.cairo.Context(self._surface)
+        # TeX coordinates to PDF?
+        self._ctx.scale(1/65536., 1/65536.)
+
+    def _shipout_box(self, cur_file, cur_line, cur_point, cur_size):
+        self._ctx.rectangle(*cur_point, *cur_size)
+        self._ctx.stroke()
+        pass
+
+    def sheet(self, tree):
+        self._ctx.show_page()
+        pass
 
 # Dict inputtag -> filename
 files = {}
@@ -92,7 +130,7 @@ lines = {}
 
 #pretty_print(data)
 #process(data, files, lines)
-dataVisitor().visit(data)
+cairoVisitor().visit(data)
 
 #print(lines)
 #with open("x", "wb") as f:
