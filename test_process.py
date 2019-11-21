e2de7dd4b3cf62adf9422abe0b096b7e44ce3cf3
Peter Turczak <peter@turczak.de>
2019-11-21 20:37:05 +0100

    Intermediate commit, at least shows something

--- test_process.py
+++ test_process.py
@@ -96,6 +96,51 @@ class dataVisitor(lark.Visitor):
     def NEWLINE(self, tree):
         print ("NEWLINE")
 
+class annotationVisistor(dataVisitor):
+    from pdf_annotate import PdfAnnotator, Location, Appearance
+
+    def __init__(self):
+        dataVisitor.__init__(self)
+        self._annotator = self.PdfAnnotator("Textbausteintest.pdf") 
+        self._page=0
+
+    def sheet(self, tree):
+        self._page += 1
+
+    def _shipout_box(self, cur_file, cur_line, cur_point, cur_size):
+        label = "{}:{}".format(self.files[cur_file], cur_line)
+        cur_pos = [x/65535 for x in cur_point]
+        cur_size = [x/65535 for x in cur_size]
+        print(cur_pos, cur_size, self._page)
+        self._annotator.add_annotation( 'square', self.Location(x1=cur_pos[1], y1=cur_pos[0], x2=cur_pos[0]+cur_size[0], y2=cur_pos[1]+cur_size[1], page=self._page), self.Appearance(stroke_color=(1, 0, 0), stroke_width=1),)
+        """
+        self._annotator.add_annotation("text",
+                self.Location(x1=cur_point[0],y1=cur_point[1],x2=cur_point[0]+cur_size[0],y2=cur_point[1]+cur_size[1], page=self._page),
+                self.Appearance(
+                    fill=[0.4, 0, 0],
+                    stroke_width=1,
+                    font_size=5,r
+                    content=label,
+                ),
+            )
+
+        self._annotator.add_annotation( 'square',
+                self.Location(x1=cur_point[0],y1=cur_point[1],x2=cur_point[0]+cur_size[0],y2=cur_point[1]+cur_size[1], page=self._page),
+    self.Appearance(stroke_color=(1, 0, 0), stroke_width=5),
+)
+
+        self._annotator.add_annotation("ink",
+                self.Location(points=[[cur_point[0],cur_point[1]],[cur_point[0]+cur_size[0],cur_point[1]+cur_size[1]]], page=self._page),
+                self.Appearance(
+                    stroke_color=(1,0,0),
+                    stroke_width=1,
+                ),
+            )
+        """
+    def done(self):
+        #self._annotator.add_annotation( 'square', self.Location(x1=50, y1=50, x2=100, y2=100, page=0), self.Appearance(stroke_color=(1, 0, 0), stroke_width=5),)
+        self._annotator.write("out.pdf")
+
 
 class cairoVisitor(dataVisitor):
     import cairo
@@ -106,6 +151,7 @@ class cairoVisitor(dataVisitor):
         self._ctx = self.cairo.Context(self._surface)
         # TeX coordinates to PDF?
         self._ctx.scale(1/65536., 1/65536.)
+
 
     def _shipout_box(self, cur_file, cur_line, cur_point, cur_size):
         self._ctx.rectangle(*cur_point, *cur_size)
@@ -128,7 +174,9 @@ lines = {}
 
 #pretty_print(data)
 #process(data, files, lines)
-cairoVisitor().visit(data)
+v=annotationVisistor()
+v.visit(data)
+v.done()
 
 #print(lines)
 #with open("x", "wb") as f:
